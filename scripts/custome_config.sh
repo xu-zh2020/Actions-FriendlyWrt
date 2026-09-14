07f5dd823d10f14bd166e08d60b4e5e49702a05d
xu-zh2020 <70309752+xu-zh2020@users.noreply.github.com>
2026-09-14 15:29:03 +0800

    Update custome_config.sh

--- scripts/custome_config.sh
+++ scripts/custome_config.sh
@@ -3,8 +3,14 @@
 sed -i -e '/CONFIG_MAKE_TOOLCHAIN=y/d' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_IB=y/# CONFIG_IB is not set/g' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_SDK=y/# CONFIG_SDK is not set/g' configs/rockchip/01-nanopi
+# ======================编译引入ImmortalWrt feeds======================
 cd friendlywrt
 
+# 【重点】删除原有自带 luci、packages 源，解决duplicate feed冲突
+sed -i '/^src-git packages/d' feeds.conf.default
+sed -i '/^src-git luci/d' feeds.conf.default
+
+#追加immortalwrt feeds源
 cat >> feeds.conf.default <<EOF
 src-git immortalwrt_packages https://github.com/immortalwrt/packages.git;openwrt-25.12
 src-git immortalwrt_luci https://github.com/immortalwrt/luci.git;openwrt-25.12
