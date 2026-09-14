e014f28adcb8be9077ad6c020a3badfbfa87857f
xu-zh2020 <70309752+xu-zh2020@users.noreply.github.com>
2026-09-14 15:10:32 +0800

    Update custome_config.sh

--- scripts/custome_config.sh
+++ scripts/custome_config.sh
@@ -3,17 +3,23 @@
 sed -i -e '/CONFIG_MAKE_TOOLCHAIN=y/d' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_IB=y/# CONFIG_IB is not set/g' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_SDK=y/# CONFIG_SDK is not set/g' configs/rockchip/01-nanopi
-# ========== 新增 ImmortalWrt feeds 源 ==========
 cd friendlywrt
 
-# 写入immortalwrt源到feeds.conf.default
 cat >> feeds.conf.default <<EOF
 src-git immortalwrt_packages https://github.com/immortalwrt/packages.git;openwrt-25.12
 src-git immortalwrt_luci https://github.com/immortalwrt/luci.git;openwrt-25.12
 EOF
 
-# 更新feeds并安装包定义
 ./scripts/feeds clean
 ./scripts/feeds update -a
-#./scripts/feeds install -a
-# ===============================================
+
+#只安装你真正需要的插件，举例 homeproxy
+./scripts/feeds install luci-app-homeproxy
+./scripts/feeds install homeproxy
+
+#把包加入编译配置，内置进固件
+cat >> configs/rockchip/01-nanopi <<EOF
+CONFIG_PACKAGE_luci-app-homeproxy=y
+CONFIG_PACKAGE_homeproxy=y
+EOF
+# ====================================================================
